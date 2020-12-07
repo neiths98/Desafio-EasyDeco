57354ab7975948db07e767da8d4f60ae12b5060a
Nathan Carvalho <nathan_santos@usp.br>
2020-12-07 16:57:13 -0300

    Melhoria de Comentários

--- js/validation.js
+++ js/validation.js
@@ -40,27 +40,30 @@ function checkInputs() {
         setError(name, "Campo não foi preenchido");
         values.push("false");
     } 
-    // nome com número
+    // Testa se nome contém um número
     else if (numbers.test(nameValue)) {
         setError(name, "Nome não pode conter números");
         values.push("false");
     }
-    // nome com caracter especial
+    // Testa se nome contém um caractere especial
     else if (especialNoSpace.test(nameValue)) {
         setError(name, "Nome não pode conter caracter especial");
         values.push("false");
     }
+    // Nome está adequado
     else {
         setSucess(name, "uhuu");
         values.push("true");
     }
 
     // Validação EMAIL
+    // Testa se e-mail está no formato correto (por meio de Regex)
     if (emailFormat.test(emailValue)) {
         setSucess(email, "uhuu");
         values.push("true");
         console.log("email ok");
     }
+    // E-mail adequado
     else {
         setError(email, "E-mail inválido");
         values.push("false");
@@ -68,43 +71,44 @@ function checkInputs() {
 
 
     // Validação SENHA
-    let success = false;
+    let success = false; 
+    // Testa se campo foi preenchido
     if (passwordValue === '') {
         setError(password, "Campo não foi preenchido");
         values.push("false");
         
     }
-    // senha curta
+    // Testa se a senha possui menos de 8 caracteres
     else if (passwordValue.length < 8) { 
         setError(password, "A senha deve conter no mínimo 8 caracteres");
         values.push("false");
     }
-    // senha longa
+    // Testa se a senha possui mais de 14 caracteres
     else if (passwordValue.length > 14) {
         setError(password, "A senha deve conter no máximo 14 caracteres");
         values.push("false");
     }
-    // não tem minúscula
-    // não tem maiúscula
+    // Testa se senha possui pelo menos um caracter maiúsculo e um minúsculo
     else if(!haveLower(passwordValue) || !haveUpper(passwordValue)) {
         setError(password, "Obrigatório pelo menos um caracter maíusculo, um minúsculo");
         values.push("false");
     }
-    // senha sem números
+    // Testa se senha contém números
     else if(!numbers.test(passwordValue)) {
         setError(password, "Obrigatório pelo menos um caracter numérico");
         values.push("false");
     }
-    // senha sem caracter especial
+    // Testa se senha contém caractere especial
     else if (!especial.test(passwordValue)) {
         setError(password, "Obrigatório pelo menos um caracter especial");
         values.push("false");
     }
-    // senha com muita repetição
+    // Testa se senha possui mais de 3 caracteres repetidos em sequência ou possui uma mesma sequência de caracteres
     else if (haveCharLoop(passwordValue)) {
         setError(password, "Não é permitido repetir a mesma sequencia de caracteres ou ter muitos caracteres repetidos");
         values.push("false");
     }
+    // Senha adequada
     else {
         setSucess(password, "uhuu");
         values.push("true");
@@ -112,18 +116,22 @@ function checkInputs() {
     }
 
     // Validação CONFIRMAÇÃO SENHA
+    // Testa se campo foi preenchido
     if (passwordValue === '') {
         setError(passwordConfirm, "Senha não foi preenchida");
         values.push("false");
     }
+    // Testa se o campo Senha está adequado 
     else if(success === false) {
         setError(passwordConfirm, "A senha não corresponde  aos requisitos");
         values.push("false");
     }
+    // Testa se a Senha tem o mesmo valor da Confirmação de Senha
     else if (passwordConfirmValue != passwordValue) {
         setError(passwordConfirm, "Senhas não coincidem");
         values.push("false");
     }
+    // Confirmação de senha adequada
     else {
         setSucess(passwordConfirm, "uhuu");
         values.push("true");
@@ -131,6 +139,7 @@ function checkInputs() {
 
 
     // Validação da validação
+    // Testa se todos os campos foram preenchidos corretamente
     if(values.every(elem => elem === "true")) {
         return true;
     }
@@ -139,8 +148,9 @@ function checkInputs() {
     }
 }
 
-// Funções de erro / acerto
+// FUNÇÕES DE ERRO/ACERTO DE VALIDAÇÃO
 
+// Adiciona a classe "wrong" para campos não adequados e adiciona mensagem de erro
 function setError(input, message) {
     const inputBox = input.parentElement;
     const small = inputBox.parentElement.querySelector('small');
@@ -151,16 +161,20 @@ function setError(input, message) {
     small.innerHTML = message;
 }
 
+// Adiciona a classe "correct" para campos adequados
 function setSucess(input, message) {
     const inputBox = input.parentElement;
     if (inputBox.parentElement.classList.contains("wrong")){
         inputBox.parentElement.classList.remove("wrong");        
     }
     inputBox.parentElement.classList.add("correct");
+    // Código para caso deseje exibir mensagem de sucesso:
+    // small.innerHTML = message;
 }
 
-// Funções para validação
+// FUNÇÕES PARA VALIDAÇÃO
 
+// Verifica se existe caractere minúsculo em uma string
 function haveLower(string) {
     let upperString = string.toUpperCase();
     if (string != upperString) {
@@ -169,6 +183,7 @@ function haveLower(string) {
     return false;
 }
 
+// Verifica se existe caractere maiúsculo em uma string
 function haveUpper(string) {
     let lowerString = string.toLowerCase();
     if (string != lowerString) {
@@ -177,6 +192,7 @@ function haveUpper(string) {
     return false;
 }
 
+// Verifica se existe repetição de cadeias de caracteres em uma string ou se existe mais de 3 caracteres iguais em sequência
 function haveCharLoop(string) {
     let vet = [];
     for (let i = 1; i < string.length; i++) {
