669b3dece815239dabab9223848f627ac5fc1033
Nathan Carvalho <nathan_santos@usp.br>
2020-12-07 14:29:49 -0300

    Criação do campo Confirme sua Senha

--- js/view-password.js
+++ js/view-password.js
@@ -1,6 +1,12 @@
 const eyeClose = document.querySelector(".eye-close");
 const eyeOpen = document.querySelector(".eye-open");
 
+const passwordConfirm = document.getElementById("password-2");
+const eyeCloseConfirm = document.querySelector(".eye-close-confirm");
+const eyeOpenConfirm = document.querySelector(".eye-open-confirm");
+
+// Senha
+
 eyeClose.addEventListener("click", function() {
 
     eyeClose.classList.remove("active");
@@ -25,6 +31,36 @@ eyeOpen.addEventListener("click", function() {
     }
     else {
         password.type = 'password';
+    }   
+    
+});
+
+// Confirmação de senha
+
+eyeCloseConfirm.addEventListener("click", function() {
+
+    eyeCloseConfirm.classList.remove("active");
+    eyeOpenConfirm.classList.add("active");
+    
+    if (passwordConfirm.type == 'password') {
+        passwordConfirm.type = 'text';
+    }
+    else {
+        passwordConfirm.type = 'password';
+    }   
+    
+});
+
+eyeOpenConfirm.addEventListener("click", function() {
+
+    eyeOpenConfirm.classList.remove("active");
+    eyeCloseConfirm.classList.add("active");
+    
+    if (passwordConfirm.type == 'password') {
+        passwordConfirm.type = 'text';
+    }
+    else {
+        passwordConfirm.type = 'password';
     }   
     
 });
